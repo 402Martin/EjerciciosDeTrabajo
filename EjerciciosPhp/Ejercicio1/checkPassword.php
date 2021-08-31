eb3d389c0c9bc0f41a4c7f7be2b445f8ce737e88
402Martin <402martin@gmail.com>
2021-08-31 19:48:13 -0300

    echos

--- EjerciciosPhp/Ejercicio1/checkPassword.php
+++ EjerciciosPhp/Ejercicio1/checkPassword.php
@@ -1,12 +1,10 @@
 <?php
-    if($_POST['password']== 'PassMoon'){
-        return 'contrasena correcta';
-    }
-    if(isset($_POST['password']) && $_POST['password'] != 'PassMoon'){
-        return 'contrasena incorrecta';
-    }
-    if(!isset($_POST['password']) && $_POST['password'] != 'PassMoon'){
-        return 'Ingresar contrasena';
-    }
-   
-?>
+if ($_POST['password'] == 'PassMoon') {
+    echo 'contrasena correcta';
+}
+if (isset($_POST['password']) && $_POST['password'] != 'PassMoon') {
+    echo 'contrasena incorrecta';
+}
+if (!isset($_POST['password'])) {
+    echo 'Ingresar contrasena';
+}
